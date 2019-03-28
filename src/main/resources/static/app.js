3722967eb1164a3d2514cc0a294cb37c30877a39
Andrey Korobitsyn <a.korobitsyn@xrm.ru>
2019-03-28 08:36:21 +0300

    impl error handling and add clear button

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,17 +1,26 @@
 angular.module('Notebook', [])
 
     /* @ngInject */
-    .controller('AppController', function ($scope, $http) {
+    .controller('AppController', function ($scope, $http, $timeout) {
 
         var NOTES_URL = 'http://localhost:8080/note';
         $scope.noteToAdd = {};
         $scope.notes = [];
+        $scope.errorMessage = "";
+        $scope.filterStr = "";
 
         $scope.search = search;
         $scope.add = add;
         $scope.remove = remove;
+        $scope.clear = clear;
 
         search();
+
+        function clear() {
+            $scope.errorMessage = "";
+            $scope.filterStr = "";
+            search();
+        }
 
         function remove(note) {
             return $http.put(NOTES_URL + '/delete', {id: note.id})
@@ -20,8 +29,13 @@ angular.module('Notebook', [])
                 }, errorCallback);
         }
 
-        function errorCallback(data, status) {
-            console.log("errorCallback", data, status);
+        //simple server error handling
+        function errorCallback(response) {
+            console.log("errorCallback", response);
+            $scope.errorMessage = response.data.message;
+            $timeout(function () {
+                $scope.errorMessage = "";
+            }, 2000);
         }
 
         function search(searchStr) {
